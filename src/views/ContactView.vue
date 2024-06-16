5c559458bf210ac1a67c58e5b34f3368804fb83a
21Coochie <brghtmalone@gmail.com>
2024-06-16 12:26:39 +0000

    Working version

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,86 +1,113 @@
 <template>
-    <NavbarComponent />
-    <div>
-        <main class="main">
-    <section class="shop py-5">
+  <NavbarComponent />
+  <div>
+    <main class="main">
+      <section class="shop py-5">
         <div class="container">
           <h2 class="text-center">Shop for textbooks and other educational resources</h2>
           <p>Find the best textbooks and educational resources for your child's needs.</p>
           <a href="https://shop.snapplify.com/" target="_blank" class="btn btn-primary">Shop Now</a>
         </div>
       </section>
-      <section class="contact" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-duration="500">
-      <div class="container">
-
-        <div class="row">
-
-          <div class="col-lg-6">
-
-            <div class="row">
-              <div class="col-md-12">
-                <div class="info-box">
-                  <i class="bx bx-map"></i>
-                  <h3>Our Address</h3>
-                  <p>Border-hub, Busia, Kenya</p>
+      <section
+        class="contact"
+        data-aos="fade-up"
+        data-aos-easing="ease-in-out"
+        data-aos-duration="500"
+      >
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-6">
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="info-box">
+                    <i class="bx bx-map"></i>
+                    <h3>Our Address</h3>
+                    <p>Border-hub, Busia, Kenya</p>
+                  </div>
                 </div>
-              </div>
-              <div class="col-md-6">
-                <div class="info-box">
-                  <i class="bx bx-envelope"></i>
-                  <h3>Email Us</h3>
-                  <!-- <p>logankhainga69@gmail.com<br> <theborderhub</p> -->
+                <div class="col-md-6">
+                  <div class="info-box">
+                    <i class="bx bx-envelope"></i>
+                    <h3>Email Us</h3>
+                    <!-- <p>logankhainga69@gmail.com<br> <theborderhub</p> -->
+                  </div>
                 </div>
-              </div>
-              <div class="col-md-6">
-                <div class="info-box">
-                  <b-icon-arrow-up></b-icon-arrow-up>
-                  <h3>Call Us</h3>
-                  <p>+254 792 516 158<br>+254 715 058 342</p>
+                <div class="col-md-6">
+                  <div class="info-box">
+                    <!-- <b-icon-arrow-up></b-icon-arrow-up> -->
+                    <h3>Call Us</h3>
+                    <p>+254 792 516 158<br />+254 715 058 342</p>
+                  </div>
                 </div>
               </div>
             </div>
 
-          </div>
-
-          <div class="col-lg-6">
-            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
-              <div class="row">
-                <div class="col-md-6 form-group">
-                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
+            <div class="col-lg-6">
+              <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+                <div class="row">
+                  <div class="col-md-6 form-group">
+                    <input
+                      type="text"
+                      name="name"
+                      class="form-control"
+                      id="name"
+                      placeholder="Your Name"
+                      required
+                    />
+                  </div>
+                  <div class="col-md-6 form-group mt-3 mt-md-0">
+                    <input
+                      type="email"
+                      class="form-control"
+                      name="email"
+                      id="email"
+                      placeholder="Your Email"
+                      required
+                    />
+                  </div>
                 </div>
-                <div class="col-md-6 form-group mt-3 mt-md-0">
-                  <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
+                <div class="form-group mt-3">
+                  <input
+                    type="text"
+                    class="form-control"
+                    name="subject"
+                    id="subject"
+                    placeholder="Subject"
+                    required
+                  />
                 </div>
-              </div>
-              <div class="form-group mt-3">
-                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
-              </div>
-              <div class="form-group mt-3">
-                <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
-              </div>
-              <div class="my-3">
-                <div class="loading">Loading</div>
-                <div class="error-message"></div>
-                <div class="sent-message">Your message has been sent. Thank you!</div>
-              </div>
-              <div class="text-center"><button type="submit">Send Message</button></div>
-            </form>
+                <div class="form-group mt-3">
+                  <textarea
+                    class="form-control"
+                    name="message"
+                    rows="5"
+                    placeholder="Message"
+                    required
+                  ></textarea>
+                </div>
+                <div class="my-3">
+                  <div class="loading">Loading</div>
+                  <div class="error-message"></div>
+                  <div class="sent-message">Your message has been sent. Thank you!</div>
+                </div>
+                <div class="text-center"><button type="submit">Send Message</button></div>
+              </form>
+            </div>
           </div>
-
         </div>
-
-      </div>
-    </section><!-- End Contact Section -->
-  </main>
-    </div>
-    <FooterComponent />
+      </section>
+      <!-- End Contact Section -->
+    </main>
+  </div>
+  <FooterComponent />
 </template>
 <script setup>
-    import FooterComponent from '@/components/FooterComponent.vue'
-    import NavbarComponent from '@/components/NavbarComponent.vue'
+import FooterComponent from '@/components/FooterComponent.vue'
+import NavbarComponent from '@/components/NavbarComponent.vue'
 </script>
 <style>
-    /*--------------------------------------------------------------
+/*--------------------------------------------------------------
 # Contact
 --------------------------------------------------------------*/
 .contact {
@@ -97,7 +124,7 @@
 
 .contact .info-box i {
   font-size: 32px;
-  color: #68A4C4;
+  color: #68a4c4;
   border-radius: 50%;
   padding: 8px;
   border: 2px dotted #a2cce3;
@@ -132,7 +159,7 @@
   font-weight: 600;
 }
 
-.contact .php-email-form .error-message br+br {
+.contact .php-email-form .error-message br + br {
   margin-top: 25px;
 }
 
@@ -153,7 +180,7 @@
 }
 
 .contact .php-email-form .loading:before {
-  content: "";
+  content: '';
   display: inline-block;
   border-radius: 50%;
   width: 24px;
@@ -173,7 +200,7 @@
 
 .contact .php-email-form input::focus,
 .contact .php-email-form textarea::focus {
-  background-color: #68A4C4;
+  background-color: #68a4c4;
 }
 
 .contact .php-email-form input {
@@ -184,15 +211,15 @@
   padding: 12px 15px;
 }
 
-.contact .php-email-form button[type=submit] {
-  background: #68A4C4;
+.contact .php-email-form button[type='submit'] {
+  background: #68a4c4;
   border: 0;
   padding: 10px 24px;
   color: #fff;
   transition: 0.4s;
 }
 
-.contact .php-email-form button[type=submit]:hover {
+.contact .php-email-form button[type='submit']:hover {
   background: #8dbad2;
 }
 
